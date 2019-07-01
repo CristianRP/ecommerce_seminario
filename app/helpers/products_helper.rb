79611f6ebc96ec3cdb4e14fde75059e6c2f561a9
Cristian Ramirez <cristian.ramirez@pdc.la>
2019-06-30 22:16:56 -0600

    20190630 221656
    Added option for dealers to see the list of products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+
+  def active(is_active)
+    content_tag(:span, nil, nil) {
+      content_tag(:i, nil, class: is_active ? 'fa fa-check' : 'fa fa-times', style: is_active ? 'color: green;' : 'color: red;')
+    }
+  end
+
 end
